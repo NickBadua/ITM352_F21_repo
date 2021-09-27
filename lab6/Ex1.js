0342da52f8faff7fe7ad83b0712aa46f8935a85b
NickBadua <nabb@hawaii.edu>
2021-09-26 20:58:06 -1000

    updates

---
+++ lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age =  21;
+var fav_num = 72;
+var day_of_birth = 25;
+var month_of_birth = 8;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
